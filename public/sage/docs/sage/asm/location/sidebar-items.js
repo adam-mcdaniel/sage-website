71c51c6989d8d1c3859d4aac13e657c8854a18f3
Adam McDaniel <adam.mcdaniel17@gmail.com>
2024-09-10 14:10:51 -0400

    Minified JS

--- public/sage/docs/sage/asm/location/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"constant":["A","B","C","D","E","F","FP","GP","REGISTERS","SP"],"enum":["Location"]};
--- public/sage/docs/sage/asm/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"enum":["Error"],"mod":["core","globals","location","std"],"trait":["AssemblyProgram"]};
